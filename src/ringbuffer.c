b5a6d6c9af9db34605deafca0291b89796683ec7
Luigi Sica Nery <luigiesn@users.noreply.github.com>
2015-12-19 19:09:27 -0200

    Changed header path on source

--- src/ringbuffer.c
+++ src/ringbuffer.c
@@ -16,7 +16,7 @@
  * this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "ringbuffer.h"
+#include "../include/ringbuffer.h"
 
 void ringbufferInit(ringbuffer_t* rb, unsigned char* buffer, unsigned char size) {
     rb->buffer = buffer;
